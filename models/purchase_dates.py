348e50d193a5207319667e68a58f2b766c287523
AfroMonkey <moisalejandro@gmail.com>
2016-07-17 12:46:45 -0500

    [IMP] Add constrains Add constrains to prevent incongruent dates

--- models/purchase_dates.py
+++ models/purchase_dates.py
@@ -6,3 +6,9 @@ class PurchaseDates(models.Model):
 
     start_date = fields.Date()
     final_date = fields.Date()
+
+    @api.one
+    @api.constrains('start_date', 'final_date')
+    def _check_dates(self):
+        if self.start_date > self.final_date:
+            raise ValidationError("The start date cannot be greater than final date")
